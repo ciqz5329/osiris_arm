b73418b9f89cae3fed934bf0d53bd88b71463ca7
acmachine <ciqz5329@163.com>
2024-12-17 01:24:37 +0800

    add cmake for arm

--- src_arm/arm-toolchain.cmake
+++ src_arm/arm-toolchain.cmake
@@ -1,9 +1,13 @@
 set(CMAKE_SYSTEM_NAME Linux)
-set(CMAKE_SYSTEM_PROCESSOR arm)
+set(CMAKE_SYSTEM_PROCESSOR aarch64)
 
+# 设置ARM编译工具链
+set(CMAKE_C_COMPILER aarch64-linux-gnu-gcc)
+set(CMAKE_CXX_COMPILER aarch64-linux-gnu-g++)
 
-# 其他配置选项
-set(CMAKE_FIND_ROOT_PATH ${TOOLCHAIN_PATH}/arm-linux-gnueabihf)
+# 设置工具链的根路径
+set(CMAKE_FIND_ROOT_PATH /usr/aarch64-linux-gnu)
+
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
