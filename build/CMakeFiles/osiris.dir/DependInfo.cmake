437ee5e02da5c1496e3338bc772f8e0cb1e18bbc
acmachine <ciqz5329@163.com>
2024-12-22 18:04:06 +0800

    update build dir

---
+++ build/CMakeFiles/osiris.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/hgfs/ShareDir/osiris-arm/src/code_generator.cc" "CMakeFiles/osiris.dir/src/code_generator.cc.o" "gcc" "CMakeFiles/osiris.dir/src/code_generator.cc.o.d"
+  "/mnt/hgfs/ShareDir/osiris-arm/src/core.cc" "CMakeFiles/osiris.dir/src/core.cc.o" "gcc" "CMakeFiles/osiris.dir/src/core.cc.o.d"
+  "/mnt/hgfs/ShareDir/osiris-arm/src/executor.cc" "CMakeFiles/osiris.dir/src/executor.cc.o" "gcc" "CMakeFiles/osiris.dir/src/executor.cc.o.d"
+  "/mnt/hgfs/ShareDir/osiris-arm/src/filter.cc" "CMakeFiles/osiris.dir/src/filter.cc.o" "gcc" "CMakeFiles/osiris.dir/src/filter.cc.o.d"
+  "/mnt/hgfs/ShareDir/osiris-arm/src/logger.cc" "CMakeFiles/osiris.dir/src/logger.cc.o" "gcc" "CMakeFiles/osiris.dir/src/logger.cc.o.d"
+  "/mnt/hgfs/ShareDir/osiris-arm/src/osiris.cc" "CMakeFiles/osiris.dir/src/osiris.cc.o" "gcc" "CMakeFiles/osiris.dir/src/osiris.cc.o.d"
+  "/mnt/hgfs/ShareDir/osiris-arm/src/utils.cc" "CMakeFiles/osiris.dir/src/utils.cc.o" "gcc" "CMakeFiles/osiris.dir/src/utils.cc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
